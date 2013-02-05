974667051317031ccf1e0eb4118332a289dcde58
Todd Sampson <tsampson@cloudspace.com>
2013-02-04 17:30:35 -0800

    Starting to add motor shield drivetrain info.  Also included android connection that I'm going to pull for motor testing.

--- toddbot.ino
+++ toddbot.ino
@@ -1,13 +1,8 @@
-// AOA1/ADK1
 #include "Arduino.h"
 #include <Max3421e.h>
 #include <Usb.h>
-#include <AndroidAccessory.h>
-#include <AFMotor.h>
-
-
-
-
+#include <AndroidAccessory.h> // ADK2011
+#include <AFMotor.h> // Motor Shield
 
 
 #define ACCESSORY_STRING_VENDOR "Todd"
@@ -16,7 +11,6 @@
 #define ACCESSORY_STRING_VERSION  "1.1"
 #define ACCESSORY_STRING_URL    "http://www.toddsampson.com"
 #define ACCESSORY_STRING_SERIAL "0000000091239123"
-
 #define MAX_PACKET_SZ 260  //256b payload + header
 
 // DRIVETRAINS (Based on Gordon McComb's Constructing Robot Bases book)
@@ -35,11 +29,13 @@
 // 10 = Three-Wheel Omnidirectional Drive
 // 11 = Four-Wheel Omnidirectional Drive
 // 12 = Synchronized Omnidirectional Drive
+#define DRIVETRAIN 6
 
+#define DIRECTION_MSG 0x2
 
-#define DRIVETARAIN 2
+AF_DCMotor motor_front(2, MOTOR12_1KHZ);
+AF_DCMotor motor_rear(3, MOTOR12_1KHZ);
 
-uint8_t cmd;
 
 AndroidAccessory acc(ACCESSORY_STRING_VENDOR,
                      ACCESSORY_STRING_NAME,
@@ -51,25 +47,90 @@ AndroidAccessory acc(ACCESSORY_STRING_VENDOR,
 void setup() {
   Serial.begin(115200);
   delay(100);
+
+  if(DRIVETRAIN == 6) {
+    motor_front.setSpeed(150);
+    motor_rear.setSpeed(150);
+    motor_front.run(RELEASE);
+    motor_rear.run(RELEASE);
+  }
+//  msg[0] = 0x2;
+//  msg[1] = 'F';
   acc.powerOn();
 }
 
 
 void loop() {
+  byte msg[2];
   if (acc.isConnected()) {
-    uint8_t msg[MAX_PACKET_SZ];
-    uint8_t outmsg[MAX_PACKET_SZ];
-    int recvLen = acc.read(msg, sizeof(msg));
-    if (recvLen > 0) {
-      // process message
- 
+    int len = acc.read(msg, sizeof(msg), 1);
+    Serial.print("Message length: ");
+    Serial.println(len, DEC);
+    if (len > 0) {
+      if (msg[0] = DIRECTION_MSG) {
+        int direction = msg[1];
+        if (direction == 'F') {
+          goForward();
+        } else if (direction == 'R') {
+          turnRight();
+        } else if (direction == 'L') {
+          turnLeft();
+        } else if (direction == 'B') {
+          goBackward();
+        } else {
+          stop();
+        }
+      }
+    } else {
+      stop();
     }
-//    acc.write(cmd);
+    msg[0] = -1;
   }
-//  acc.adkEventProcess();
+  else {
+    stop();
+  }
+  delay(100);
 }
 
-void 
+void goForward() {
+  if(DRIVETRAIN == 6) {
+    motor_front.run(RELEASE);
+    motor_rear.run(FORWARD);
+  }
+}
+
+void goBackward() {
+  if(DRIVETRAIN == 6) {
+    motor_front.run(RELEASE);
+    motor_rear.run(BACKWARD);
+  }  
+}
+
+void turnRight() {
+  if(DRIVETRAIN == 6) {
+    motor_front.run(FORWARD);
+    motor_rear.run(FORWARD);
+  }  
+}
+
+void turnLeft() {
+  if(DRIVETRAIN == 6) {
+    motor_front.run(BACKWARD);
+    motor_rear.run(FORWARD);
+  }  
+}
+
+void stop() {
+  if(DRIVETRAIN == 6) {
+    motor_front.run(RELEASE);
+    motor_rear.run(RELEASE);
+  }
+}
+
+
+// TODO: Create Library for MegaMoto Shields
+
+
 
 
 
